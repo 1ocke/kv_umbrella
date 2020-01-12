4b866903bdd8867f3eaed7d3d7857aee3cbc4b2a
Andrew Zabelin <a.zabelin1@yandex.ru>
2020-01-12 17:58:24 +0300

    add TaskSupervisor

--- apps/kv_server/lib/kv_server/application.ex
+++ apps/kv_server/lib/kv_server/application.ex
@@ -7,8 +7,8 @@ defmodule KVServer.Application do
 
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: KVServer.Worker.start_link(arg)
-      # {KVServer.Worker, arg}
+      {Task.Supervisor, name: KVServer.TaskSupervisor},
+      Supervisor.child_spec({Task, fn -> KVServer.accept(4040) end}, restart: :permanent)
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
